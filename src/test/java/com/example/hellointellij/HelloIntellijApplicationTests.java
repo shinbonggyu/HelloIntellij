5fe01a2a512acb2077c1e109f341d2bfd76b71fa
bonggyu <bgshin96@naver.com>
2022-08-11 17:34:39 +0900

    HelloIntellij

---
+++ src/test/java/com/example/hellointellij/HelloIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellointellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloIntellijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
